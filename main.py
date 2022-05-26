88c724565a834c17bfcc58c62835e4cb385ad55a
HungDaoHD <hung.daotuan1991@gmail.com>
2022-05-26 18:53:13 +0700

    aaa

--- main.py
+++ main.py
@@ -1,25 +1,98 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from app.routers import Excel2Sav, BaeminCheck
 
 
+from starlette.responses import FileResponse
+import traceback
+from app.classes.AP_DataConverter import APDataConverter
+
+
 templates = Jinja2Templates(directory='app/frontend/templates')
 
 app = FastAPI()
 
-app.include_router(Excel2Sav.router)
+# app.include_router(Excel2Sav.router)
 app.include_router(BaeminCheck.router)
 
 app.mount('/static', StaticFiles(directory='app/frontend/static'), name='static')
-app.mount('/convert-sav/static', StaticFiles(directory='app/frontend/static'), name='static')
+# app.mount('/convert-sav/static', StaticFiles(directory='app/frontend/static'), name='static')
 app.mount('/baemin-check/static', StaticFiles(directory='app/frontend/static'), name='static')
 
 
 @app.get('/', response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse('myindex.html', {'request': request})
+
+
+
+
+
+
+@app.get('/convert-sav', response_class=HTMLResponse)
+async def load_xlsx(request: Request):
+    return templates.TemplateResponse('load_xlsx.html', {'request': request})
+
+
+
+@app.post('/convert-sav', response_class=HTMLResponse)
+async def convert_sav(file: UploadFile, request: Request):
+    try:
+        apCvt = APDataConverter()
+        apCvt.load(file)
+        apCvt.toSav()
+        apCvt.getMRSetSyntax()
+        apCvt.zipfiles()
+
+        return FileResponse(apCvt.zipName, filename=apCvt.zipName)
+
+        # return templates.TemplateResponse('successful.html', {
+        #     'request': request,
+        #     'strTask': 'Convert to SAV',
+        #     'strFileName': str(apCvt.strFileName).replace('.xlsx', '')
+        # })
+
+    except Exception:
+        print(traceback.format_exc())
+
+        return templates.TemplateResponse('error.html', {
+            'request': request,
+            'strTask': 'Convert to SAV',
+            'strErr': 'Error!!! Please check your upload file.'
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
