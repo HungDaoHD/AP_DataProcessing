48b5169b28088df18a4c746a33ee613c3c4b1d6f
HungDaoHD <hung.daotuan1991@gmail.com>
2022-05-27 16:15:24 +0700

    aaaaaaa

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ async def index(request: Request):
     return templates.TemplateResponse('myindex.html', {'request': request})
 
 
+
+
+
 import uvicorn
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
