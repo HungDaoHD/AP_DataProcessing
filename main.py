182b4da1a851975209cf430e351ad399cd173e3d
HungDaoHD <hung.daotuan1991@gmail.com>
2022-06-01 14:31:53 +0700

    aaa

--- main.py
+++ main.py
@@ -39,6 +39,6 @@ async def index(request: Request):
     return templates.TemplateResponse('chart.html', {'request': request})
 
 
-import uvicorn
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# import uvicorn
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
