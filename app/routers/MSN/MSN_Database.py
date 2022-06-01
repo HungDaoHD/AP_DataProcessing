2c86e3be4e0da3c3a0dd8ca60cd19a6c2384bb3c
HungDaoHD <hung.daotuan1991@gmail.com>
2022-06-01 14:43:34 +0700

    aaa

--- app/routers/MSN/MSN_Database.py
+++ app/routers/MSN/MSN_Database.py
@@ -5,7 +5,8 @@ from bson.objectid import ObjectId
 class MsnPrj:
 
     def __init__(self):
-        MONGO_DETAILS = 'mongodb://localhost:27017'
+        # MONGO_DETAILS = 'mongodb://localhost:27017'
+        MONGO_DETAILS = 'mongodb+srv://hungdao:Hung123456@cluster0.m1qzy.mongodb.net/test'
 
         client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
