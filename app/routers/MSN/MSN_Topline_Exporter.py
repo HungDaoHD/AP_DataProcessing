32bfd598fc8f0b1ce4f3b2562fedfa4b187edc0d
HungDaoHD <hung.daotuan1991@gmail.com>
2022-06-14 09:18:41 +0700

    aaa

--- app/routers/MSN/MSN_Topline_Exporter.py
+++ app/routers/MSN/MSN_Topline_Exporter.py
@@ -47,9 +47,9 @@ class ToplineExporter:
             fileName = self.toplineName
 
             # self.toSummary(wb, self.dictTtest)
-            self.toTabulation(wb, self.dictTtest)
-            # self.toOlJrSummary(wb, self.dictTtest, 'OL')
-            # self.toOlJrSummary(wb, self.dictTtest, 'JR')
+            # self.toTabulation(wb, self.dictTtest)
+            self.toOlJrSummary(wb, self.dictTtest, 'OL')
+            self.toOlJrSummary(wb, self.dictTtest, 'JR')
             # self.toUandA(wb, self.dictUA)
             # self.toCorr(wb)
 
