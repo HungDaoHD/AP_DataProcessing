da86dcee14e73d5d41ded9d9124f6ebe63f19f5b
HungDaoHD <hung.daotuan1991@gmail.com>
2022-06-15 15:41:29 +0700

    aaaa

--- app/routers/MSN/MSN_Topline_Exporter.py
+++ app/routers/MSN/MSN_Topline_Exporter.py
@@ -56,13 +56,13 @@ class ToplineExporter:
             wb = openpyxl.Workbook()
             fileName = self.toplineName
 
-            self.toSummary(wb, self.dictTtest)
-            self.toTabulation(wb, self.dictTtest)
-            self.toOlJrSummary(wb, self.dictTtest, 'OL')
-            self.toOlJrSummary(wb, self.dictTtest, 'JR')
-            # self.toUandA(wb, self.dictUA)
-            self.toCorr(wb)
-            
+            # self.toSummary(wb, self.dictTtest)
+            # self.toTabulation(wb, self.dictTtest)
+            # self.toOlJrSummary(wb, self.dictTtest, 'OL')
+            # self.toOlJrSummary(wb, self.dictTtest, 'JR')
+            self.toUandA(wb, self.dictUA)
+            # self.toCorr(wb)
+
             wb.remove(wb['Sheet'])
 
             wb.save(fileName)
